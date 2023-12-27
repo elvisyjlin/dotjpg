e9845e5ed039ec9ece64916ecec9ffeae057d93a
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2023-12-27 19:13:25 +0800

    Support instagram

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      "padding": "1rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
