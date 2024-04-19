ddd7cdcef2bfb6c2423610b2a9ea276620d6a0ea
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2024-04-19 11:06:03 +0800

    Clear url button

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { FC, useState } from "react";
 import { capitalize } from "../utils";
 import MessageModal from "./modal";
+import { X } from "@phosphor-icons/react";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -81,14 +82,24 @@ const Hero: FC<HeroProps> = ({ selectedPlatform }) => {
           <h1 className="mt-4 sm:mt-12 text-3xl font-bold">{title}</h1>
           <p className="text-slate-600">{description}</p>
           <form className="mt-2 flex flex-col sm:flex-row gap-2 w-full sm:w-auto" onSubmit={handleSubmit}>
-            <div className="relative">
+            <div className="relative overflow-hidden">
               <input
-                className="border border-slate-300 rounded-md px-4 py-2 w-full sm:w-[400px]"
+                className="px-4 py-2 w-full border border-slate-300 rounded-md sm:w-[400px]"
                 type="text"
                 placeholder={placeholder}
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
               />
+              <button
+                className="absolute top-1.5 right-[4.1rem] text-slate-400 bg-white border border-slate-400 
+                  hover:text-slate-800 hover:border-slate-800 rounded-md text-sm p-1.5"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setInput("");
+                }}
+              >
+                <X size={16} />
+              </button>
               <button
                 className="absolute top-1.5 right-2 text-white bg-slate-400 hover:bg-slate-500 border 
                   border-slate-400 rounded-md px-2 p-1 text-sm"
